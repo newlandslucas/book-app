9f1ec12ec976b97de9da6ac0e4560130fc3a6ff0
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-12-19 00:20:53 -0300

    feature: API config

---
+++ src/service/API/APIConfig.js
@@ -0,0 +1,4 @@
+export const API_KEY = "AIzaSyBYWBxXu0P7eETdZTnRcnZYnt0VeoJeulM"
+export const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books"
+export const GET_BOOKS_BY_NAME_ENDPOINT = "/v1/volumes?q="
+export const KEY_HEADER = "&key" + API_KEY
